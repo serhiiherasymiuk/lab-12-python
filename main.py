79a466f1934e8b6d488bc5f69af0e92ff81fa224
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2024-02-14 22:28:11 +0200

    Modify sum_int_number method to use doctest and try except

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def sum_int_number(number):
-    num = 1
-    sum_result = 0
-    while num < number + 1:
-        sum_result = sum_result + num
-        num = num + 1
+    try:
+        sum_result = sum(range(number + 1))
+    except TypeError:
+        sum_result = 0
     return sum_result
 
 
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
     print(sum_int_number(10))
 
 
